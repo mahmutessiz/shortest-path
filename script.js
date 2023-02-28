f4721d98c9ec5d5ef27e03af85c7e4144dd48024
Mahmut Eşsiz <mahmutessiz71@gmail.com>
2023-02-28 17:31:24 +0300

    another fail day day-3

--- script.js
+++ script.js
@@ -3,70 +3,69 @@ const ul1 = document.querySelector("#ul").children;
 const start = document.querySelector("#start");
 const finish = document.querySelector("#finish");
 
- 
+const red = "#ff0000";
+let magicNumber;
 
- function choose(event) {
-    console.log(event);
 
-   if (event.target.id ==  "ul") {
-       return;
-    }else{
-        console.log(event.target.innerText)
-       event.target.style.backgroundColor= "#ff0000";
-       event.target.classList.add("target");
-    }
+function choose(event) {
+     magicNumber = Number(event.target.innerText);
+     console.log(event)
 
-   for (let i = 0; i < ul1.length; i++) {
+    if (event.target.id == "ul") {
+        return;
+    } else if (event.target.id == !"wall") {
+            event.target.style.backgroundColor = red;
+             event.target.classList.add("target"); 
+        }
+    
 
-            if (ul1[i].innerText == event.target.innerText) {
-             console.log("deneme");
- 
-            }if (Number(ul1[i].innerHTML) + 1 == event.target.innerHTML) {
+    for (let i = 0; i < ul1.length; i++) {
+
+        
+
+
+          if (ul1[i].innerText == magicNumber && magicNumber > 0 && ul1[i].id== !"wall") {
+            magicNumber--;
+            console.log(magicNumber);
+            console.log(ul1[i]);
+            
+        
+        if (ul1[i].className == "target") {
+            console.log(i);
+        }
+            
+        } 
+
+
+
+
+/*         if (ul1[i].innerHTML == "hi") {
+            console.log(ul1[i - 1].innerHTML);
+
+            for (let index = 100; index > -0; index--) {
+
+                if (i - index > 0 && ul1[index].id == !"wall") {
+
+                     if (Number(ul1[index].innerHTML) < Number(ul1[i].innerHTML) ) {
+                       ul1[index].innerHTML = ul1[i].innerHTML;   
+                    } 
+                    ul1[index].innerHTML = ul1[i].innerHTML;
+                }
+                
+            }
+        } */
+
+        /* if (Number(ul1[i].innerHTML) + 1 == event.target.innerHTML) {
+
                 console.log("idene");
                 console.log(event.target.style);
                 ul1[i].style.backgroundColor= "#ff0000";
-             }
-            
-        }
 
+             }  */
+    }
 }
 
-ul.addEventListener('click', choose);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+ul.addEventListener("click", choose);
 
 /* if (finish.innerHTML == "0") {
     ul[88].innerHTML += Number(finish.innerHTML) + 1;
